21b937a7f1a2f7bc4e0ed0e828056d6262afd855
VAsh19 <73327898+VAsh19@users.noreply.github.com>
2025-05-04 10:25:59 +0530

    Add files via upload

---
+++ paymentService2/src/main/java/com/microservice/paymentService/filter/InternalCallFilter.java
@@ -0,0 +1,30 @@
+package com.microservice.paymentService.filter;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class InternalCallFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        String internalCallHeader = request.getHeader("X-Internal-Call");
+
+        if (!"true".equalsIgnoreCase(internalCallHeader)) {
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, "Forbidden");
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
